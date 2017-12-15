cdda62ffbbf1abc331dfdc78d4e2a8913d87af42
Juraj Orszag <orszag.juraj@gmail.com>
2017-12-15 17:08:42 +0100

    Project 4 - incorporated hand outline plot

--- project4/js/plots.js
+++ project4/js/plots.js
@@ -2,12 +2,11 @@ d3.select(window).on('load', init);
 
 function init() {
 
+    var defaultHandId = "id1";
     var svg = d3.select('#scatter-plot');
     var margin = {top: 50, right: 50, bottom: 50, left: 50};
     var width = +svg.node().getBoundingClientRect().width;
     var height = +svg.node().getBoundingClientRect().height;
-
-    var g = svg.append("g")
 
     d3.tsv("handsPCA.txt", function(error, data) {
         if(error) throw error;
@@ -33,10 +32,6 @@ function init() {
             })
             .attr("fill", "red")
             .attr("stroke", "white")
-            .append("svg:title")
-            .text(function(d) {
-                return d.V1;
-            })
             .on("mouseover", handleMouseOver);
 
         var xAxis = d3.axisBottom()
@@ -60,8 +55,56 @@ function init() {
             .attr("transform","translate(" + margin.left + ", 0)");
 
         function handleMouseOver(d,i) {
-
+            updateHand(d.id);
         }
 
     });
+
+    d3.tsv('hands.txt', function(error, data) {
+        if (error) throw error;
+
+        svg2 = d3.select('#hand_outline');
+        var width = +svg2.node().getBoundingClientRect().width;
+        var height = +svg2.node().getBoundingClientRect().height;
+        var padding = 30;
+
+        xScale = d3.scaleTime()
+            .domain(d3.range(0, 1.5))
+            .range([padding, width - padding]);
+
+        yScale = d3.scaleLinear()
+            .domain([0 , 0.8 ])
+            .range([height - padding, padding]);
+
+        handsData = data;
+        drawHand(defaultHandId, data);
+
+    });
+
+    function drawHand(id) {
+        //Create line
+        svg2.append("path")
+            .datum(handsData)
+            .attr("class", "line")
+            .attr("d", getLine(id))
+            .attr("stroke" , "grey")
+            .attr("stroke-width", "5px")
+            .attr("fill", "none")
+            .attr("transform", "translate(" + (-width / 6) + "," + (height / 6 ) + ")");
+    }
+
+    function updateHand(id) {
+        //Update line
+        svg2.select("path")
+            .attr("d", getLine(id));
+    }
+
+    function getLine(id) {
+        //Define line generator
+        return d3.line()
+            .defined(function(d) { return d.id === id; })
+            .x(function(d) { return xScale(d.x); })
+            .y(function(d) { return yScale(d.y); })
+            .curve(d3.curveCatmullRom);
+    }
 }
