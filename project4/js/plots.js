c28769989513a1097a5cb366d55e5443c9b54ea2
Juraj Orszag <orszag.juraj@gmail.com>
2017-12-16 20:50:22 +0100

    Project 4 - Support for changing which two PCA-variables are displayed

--- project4/js/plots.js
+++ project4/js/plots.js
@@ -11,24 +11,35 @@ function init() {
     d3.tsv("handsPCA.txt", function(error, data) {
         if(error) throw error;
 
-        var x = d3.scaleLinear()
+        handsPCAdata = data;
+
+        var headers = d3.keys(data[0]);
+        populatePCASelectors(headers);
+
+        select = document.getElementById("pca1-select");
+        select2 = document.getElementById("pca2-select");
+
+        var selectedVar = select.options[select.selectedIndex].value;
+        var selectedVar2 = select.options[select2.selectedIndex].value;
+
+        x = d3.scaleLinear()
             .domain([-0.8, 0.8])
             .range([margin.left, width - margin.right]);
 
-        var y = d3.scaleLinear()
+        y = d3.scaleLinear()
             .domain([0.8, -0.5])
             .range([margin.top, height - margin.bottom]);
 
         svg.selectAll('circle')
-            .data(data)
+            .data(handsPCAdata)
             .enter()
             .append('circle')
             .attr("r", "5px")
             .attr("cx", function (d) {
-                return x(d.V1) + "px";
+                return x(d[selectedVar]) + "px";
             })
             .attr("cy", function(d) {
-                return y(d.V2) + "px";
+                return y(d[selectedVar2]) + "px";
             })
             .attr("fill", "red")
             .attr("stroke", "white")
@@ -54,10 +65,6 @@ function init() {
             .call(yAxis)
             .attr("transform","translate(" + margin.left + ", 0)");
 
-        function handleMouseOver(d,i) {
-            updateHand(d.id);
-        }
-
     });
 
     d3.tsv('hands.txt', function(error, data) {
@@ -66,20 +73,24 @@ function init() {
         svg2 = d3.select('#hand_outline');
         var width = +svg2.node().getBoundingClientRect().width;
         var height = +svg2.node().getBoundingClientRect().height;
-        var padding = 30;
+        var margin = {top: 30, right: 30, bottom: 50, left: 30};
 
         xScale = d3.scaleTime()
             .domain(d3.range(0, 1.5))
-            .range([padding, width - padding]);
+            .range([margin.left, width - margin.right]);
 
         yScale = d3.scaleLinear()
             .domain([0 , 0.8 ])
-            .range([height - padding, padding]);
+            .range([height - margin.top, margin.bottom]);
 
         handsData = data;
         drawHand(defaultHandId, data);
 
     });
+
+    function handleMouseOver(d,i) {
+        updateHand(d.id);
+    }
 
     function drawHand(id) {
         //Create line
@@ -107,4 +118,40 @@ function init() {
             .y(function(d) { return yScale(d.y); })
             .curve(d3.curveCatmullRom);
     }
+
+}
+
+function updatePoints() {
+    var var1 = select.options[select.selectedIndex].value;
+    var var2 = select.options[select2.selectedIndex].value;
+
+    d3.select('#scatter-plot')
+        .selectAll('circle')
+        .transition()
+        .ease(d3.easeCubic)
+        .attr("cx", function (d) {
+            return x(d[var1]) + "px";
+        })
+        .attr("cy", function(d) {
+            return y(d[var2]) + "px";
+        });
+}
+
+function populatePCASelectors(vars) {
+    var select = document.getElementById("pca1-select");
+    var select2 = document.getElementById("pca2-select");
+    for(var i=0; i < 39; i++) {
+        var elem = vars[i];
+        select.options.add(createOpt(elem));
+        select2.options.add(createOpt(elem));
+    }
+    select.selectedIndex = 0;
+    select2.selectedIndex = 1;
+}
+
+function createOpt(elem) {
+    var opt = document.createElement("option");
+    opt.value= elem;
+    opt.text = elem;
+    return opt;
 }
