dc01dcf8e5bea7ce5f426b23351f980935e8ee6c
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2017-12-19 11:35:48 +0100

    Added hand PCA animation by Kamma.

--- project4/js/plots.js
+++ project4/js/plots.js
@@ -8,8 +8,14 @@ function init() {
     var width = +svg.node().getBoundingClientRect().width;
     var height = +svg.node().getBoundingClientRect().height;
     var selectedHands = {};
+    var handsPCAdata = {};
 
 
+    var piyg = d3.scaleSequential(d3.interpolatePiYG)
+        .domain([1,40]);
+
+
+    console.log(piyg(2));
     d3.tsv("handsPCA.txt", function(error, data) {
         if(error) throw error;
 
@@ -70,18 +76,31 @@ function init() {
             .attr("class", "axis")
             .call(yAxis)
             .attr("transform","translate(" + margin.left + ", 0)");
-
     });
 
-    d3.tsv('hands.txt', function(error, data) {
+    d3.queue()
+      .defer(d3.tsv, 'hands.txt')
+      .defer(d3.tsv, 'handsPCA.txt')
+      .await(ready);
+
+    function ready(error, data, handsPCA) {
+
         if (error) throw error;
 
+        handsPCA.sort(function(a, b){
+            return a.V1 - b.V1;
+        });
+        var ids = handsPCA.map(function(a) {
+            return a.id;
+        });
+
         svg2 = d3.select('#hand_outline');
+        svg3 = d3.select('#overlayHands');
         var width = +svg2.node().getBoundingClientRect().width;
         var height = +svg2.node().getBoundingClientRect().height;
         var margin = {top: 30, right: 30, bottom: 50, left: 30};
 
-        xScale = d3.scaleTime()
+        xScale = d3.scaleLinear()
             .domain(d3.range(0, 1.5))
             .range([margin.left, width - margin.right]);
 
@@ -91,8 +110,54 @@ function init() {
 
         handsData = data;
         drawHand(defaultHandId, data);
+        drawHandOverlay(defaultHandId, data);
+        d3.select("#clickline").on('click', function(){
+          update1(ids);
+        });
+    }
 
-    });
+    function update1(ids) {
+
+        var counter = 0;
+        for (var i = 0; i < ids.length; i++) {
+            //lineKW.defined(function (d) {return d.id == new_hand_id;})
+            svg3.select("path")
+                .datum(handsData)
+                .transition()
+                .duration(500)
+                .ease(d3.easeLinear)
+                .delay(500 * i)
+                .attr("d", getLine(ids[i]))
+                .on("end", function(d){
+                    counter = counter + 1; // safe as counter++;
+                    var cloned = cloneSelection(svg3, path1, counter);
+                });
+        }
+    }
+
+    function cloneSelection(appendTo, toCopy, hnr) {
+        toCopy.each(function () {
+                var clone = svg3.node()
+                    .appendChild(this.cloneNode(true));
+              console.log(hnr);
+                d3.select(clone)
+                    .attr("class", "clone")
+                    .attr("id", "clone-" + hnr)
+                    .attr("stroke", piyg(hnr))
+                    .attr("stroke-width",4);
+        });
+        return appendTo.selectAll('.clone');
+    }
+
+    function getLine(id) {
+        //Define line generator
+        return d3.line()
+            .defined(function(d) { return d.id === id; })
+            .x(function(d) { return xScale(d.x); })
+            .y(function(d) { return yScale(d.y); })
+            .curve(d3.curveCatmullRom);
+    }
+
 
     function handleMouseClick(d, i) {
 
@@ -114,7 +179,7 @@ function init() {
         svg.select('#point-' + d.id)
             .attr("fill", function(d){
                 if(selectedHands[d.id] === true){
-                  return color;
+                    return color;
                 }
                 return "red"
             });
@@ -136,20 +201,24 @@ function init() {
             .attr("fill", "none")
             .attr("transform", "translate(" + (-width / 6) + "," + (height / 6 ) + ")");
     }
+    function drawHandOverlay(id) {
+        //Create line
+        path1 = svg3.append("svg")
+            .append("path")
+            .datum(handsData)
+            .attr("class", "line")
+            .attr("d", getLine(id))
+            .attr("id", "dynamicLine")
+            .attr("stroke" , "blue")
+            .attr("stroke-width", "5px")
+            .attr("fill", "none")
+            .attr("transform", "translate(" + (-width / 6) + "," + (height / 6 ) + ")");
+    }
 
     function updateHand(id) {
         //Update line
         svg2.select("path")
             .attr("d", getLine(id));
-    }
-
-    function getLine(id) {
-        //Define line generator
-        return d3.line()
-            .defined(function(d) { return d.id === id; })
-            .x(function(d) { return xScale(d.x); })
-            .y(function(d) { return yScale(d.y); })
-            .curve(d3.curveCatmullRom);
     }
 
 }
